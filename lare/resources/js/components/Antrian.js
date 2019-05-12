2f94cbc955c2b4a560ff1e7650d64731c76fa0f5
unknown <fadhelijlalfalah@gmail.com>
2019-05-13 00:22:16 +0700

    antrian-sedikit

--- lare/resources/js/components/Antrian.js
+++ lare/resources/js/components/Antrian.js
@@ -4,9 +4,7 @@ import axios from 'axios'
 import Pusher from 'pusher-js'
 // import  from 'peer'
 var kotak={
-    backgroundColor:'red',
     width:'100px',
-    color:'white',
     height:'100px',
     margin:'0 auto',
     fontSize:'20px',
@@ -50,9 +48,52 @@ export default class Antrian extends Component {
         if(this.state.isLoad) return null
         return(
             <>
+             <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+        <div class="dropdown-menu" aria-labelledby="dropdown01">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"/>
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<main role="main">
+
+  <div class="jumbotron">
+    <div class="container">
             <h1 className="text-center">Antrian</h1>
-            <div style={kotak} onClick={this.daftar} className="d-flex justify-content-center align-items-center">{this.state.no_antrian} </div>
-            <button type="button" onClick={this.daftar} class="btn btn-cyan btn-sm">+ Daftar</button>
+            <p>
+            <div style={kotak} onClick={this.daftar} className="bg-info text-white d-flex justify-content-center align-items-center">{this.state.no_antrian} </div>
+            </p>
+            <p>
+            <button type="button" onClick={this.daftar} class="btn btn-cyan btn-lg btn-block">+ Daftar</button>
+            </p>
+            </div>
+            </div>
+            </main>
             </>
         )
         
